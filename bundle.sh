0cd2c8e580501ff907388c1e5fd1e0abb6476cb4
Jesus G <jesus.gomez@jitterbit.com>
2021-03-23 22:45:05 -0400

    chore(workflow) debug yq

--- bundle.sh
+++ bundle.sh
@@ -11,8 +11,9 @@ for chart in charts/*; do
   # See https://mikefarah.gitbook.io/yq/commands/merge
   # and https://mikefarah.gitbook.io/yq/usage/convert
   yq merge --overwrite --tojson definitions.schema.yaml "${chart}"/values.schema.yaml > "${chart}"/values.schema.json
-  yq eval-all --tojson 'select(fileIndex == 0) * select(filename == "definitions.schema.yaml")' "${chart}"/values.yaml definitions.schema.yaml > "{chart}"/values.schema.new.json
-  sdiff -w 220  "{chart}"/values.schema.json "{chart}"/values.schema.new.json
+  cat "${chart}"/values.schema.json
+  #yq eval-all --tojson 'select(fileIndex == 0) * select(filename == "definitions.schema.yaml")' "${chart}"/values.yaml definitions.schema.yaml > "{chart}"/values.schema.new.json
+  #sdiff -w 220  "{chart}"/values.schema.json "{chart}"/values.schema.new.json
 done
 
 echo "done"
