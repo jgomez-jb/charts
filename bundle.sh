0bed1f1c94c64773bba4a087b2c5ea61d3cb338c
Jesus G <jesus.gomez@jitterbit.com>
2021-03-25 16:11:11 -0400

    chore(workflow) debug yq

--- bundle.sh
+++ bundle.sh
@@ -20,6 +20,7 @@ for chart in charts/*; do
     touch "$JSON_SCHEMA"
   fi
   yq eval-all --tojson "select(fileIndex == 0) * select(filename == \"$YAML_SCHEMA\")" definitions.schema.yaml "${chart}"/values.schema.yaml > "$JSON_SCHEMA"
+  cat "$JSON_SCHEMA"
 done
 
 echo "done"
