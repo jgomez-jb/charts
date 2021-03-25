45b880904b14544bd1d7556814b0f7d68a94ce64
Jesus G <jesus.gomez@jitterbit.com>
2021-03-25 11:57:59 -0400

    chore(workflow) debug yq

--- bundle.sh
+++ bundle.sh
@@ -10,10 +10,9 @@ for chart in charts/*; do
   # and output the merged YAML document as JSON.
   # See https://mikefarah.gitbook.io/yq/commands/merge
   # and https://mikefarah.gitbook.io/yq/usage/convert
-  yq merge --overwrite --tojson definitions.schema.yaml "${chart}"/values.schema.yaml > "${chart}"/values.schema.json
-  cat "${chart}"/values.schema.json
-  #yq eval-all --tojson 'select(fileIndex == 0) * select(filename == "definitions.schema.yaml")' "${chart}"/values.yaml definitions.schema.yaml > "{chart}"/values.schema.new.json
-  #sdiff -w 220  "{chart}"/values.schema.json "{chart}"/values.schema.new.json
+  #yq merge --overwrite --tojson definitions.schema.yaml "${chart}"/values.schema.yaml > "${chart}"/values.schema.json
+  #cat "${chart}"/values.schema.json
+  yq eval-all --tojson 'select(fileIndex == 0) * select(filename == "charts/agent-group/values.schema.yaml")' definitions.schema.yaml charts/agent-group/values.schema.yaml > "{chart}"/values.schema.json
 done
 
 echo "done"
