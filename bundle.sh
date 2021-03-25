ac2d9b53bee6f42dfcece6b23d411504f5c5acfa
Jesus G <jesus.gomez@jitterbit.com>
2021-03-25 14:43:29 -0400

    chore(workflow) debug yq

--- bundle.sh
+++ bundle.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-set -eo pipefail
+set -xeo pipefail
 
 echo -n "Bundling all of the charts..."
 
 # Loop through every chart
 for chart in charts/*; do
+  echo $chart
+  ls  ${chart}/values.schema.yaml
   # Combine the definitions.schema.yaml and charts/$chart/values.schema.yaml files
   # with the charts/$chart/values.schema.yaml file taking precedence
   # and output the merged YAML document as JSON.
   # See https://mikefarah.gitbook.io/yq/commands/merge
   # and https://mikefarah.gitbook.io/yq/usage/convert
-  yq eval-all --tojson 'select(fileIndex == 0) * select(filename == "${chart}/values.schema.yaml")' definitions.schema.yaml "${chart}"/values.schema.yaml > "{chart}"/values.schema.json
+  JSON_SCHEMA="${chart}/values.schema.json"
+  ls "$JSON_SCHEMA"
+  if [ ! -f "$JSON_SCHEMA" ] 
+  then
+    echo "File $FILE does not exist"
+    touch "$JSON_SCHEMA"
+    ls "$JSON_SCHEMA"
+  fi
+  yq eval-all --tojson 'select(fileIndex == 0) * select(filename == \"$chart\"/values.schema.yaml)' definitions.schema.yaml "${chart}"/values.schema.yaml > "${chart}"/values.schema.json
 done
 
 echo "done"
